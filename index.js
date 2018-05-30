58226b0b85f449f67a8e7514a01fdae716568aa3
Randall Robinson <keith_adams007@yahoo.com>
2018-05-30 21:05:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ class Neighborhood {
   
   meals() {
     return store.deliveries.filter(delivery => {
-      if ()
+      if (delivery.neighborhoodId === this.id) {
+        return delivery.mealId
+      }
     }) 
   } 
 }
