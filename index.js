f0900459c8a18cce5f27ca81e054f06e3c3a205b
Randall Robinson <keith_adams007@yahoo.com>
2018-05-30 20:08:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,66 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0
+
+class Neighborhood {
+  constructor(name){
+    this.name = name
+    this.id = ++neighborhoodId
+  }
+  
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.neighborhoodId === this.id
+    })
+  }
+  
+  customers() {
+    return store.customers.filter(customer => {
+      return customer.neighborhoodId === this.id 
+    })
+  }
+  
+  meals() {
+    
+  }
+}
+
+let customerId = 0
+
+class Customer {
+  constructor(name, neighborhoodId) {
+    this.name = name
+    this.neighborhoodId = neighborhoodId
+    this.id = ++customerId
+  }
+  
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.customerId === this.id 
+    })
+  }
+  
+  meals() {
+    return store.meals.filter(meal => {
+      return meal.customerId === this.id 
+    })
+  }
+  
+  totalSpent() {
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+  
